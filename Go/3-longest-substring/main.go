e4bb2ba11f68cf87c1531cc50441d423ca3803f2
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-06-09 16:43:22 +0700

    refactor: deleting println

--- Go/3-longest-substring/main.go
+++ Go/3-longest-substring/main.go
@@ -1,7 +1,6 @@
 package longestsubstring
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -13,13 +12,9 @@ func lengthOfLongestSubstring(s string) int {
 		if !strings.Contains(strings.Join(sub, ""), string(char)) {
 			sub = append(sub, string(char))
 
-			fmt.Printf("Char: %v, Sub: %v \n", string(char), sub)
-
 			result = max(result, len(sub))
 		} else {
 			cut := strings.Index(strings.Join(sub, ""), string(char))
-
-			fmt.Printf("Char: %v, Sub: %v, Cut: %v \n", string(char), sub, cut)
 
 			sub = append(sub[cut+1:], string(char))
 		}
